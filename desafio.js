52265f92b0a58bac54bd26c4e464b78809f3001f
Marcio Hernani <marcio.hernani@gmail.com>
2025-07-09 10:55:48 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,32 @@
+// Classificador de nível herói
+
+let nome = "Gandalf";
+let xp = 100; 
+
+
+// Verifica se a experiência é menor ou igual a 1000
+if (xp <= 1000) {
+  console.log("O herói de nome " + nome + "e está no nível Ferro");
+} 
+else if ((xp >= 1001 ) && (xp < 2000)) {
+  console.log("O herói de nome " + nome + "e está no nível Bronze");
+}
+else if ((xp >= 2001) && (xp < 5000)) {
+  console.log("O herói de nome " + nome + "e está no nível Prata");
+}
+else if ((xp >= 5001) && (xp < 7000)) {
+  console.log("O herói de nome " + nome + "e está no nível Ouro");
+}
+else if ((xp >= 7001) && (xp < 8000)) {
+  console.log("O herói de nome " + nome + "e está no nível Platina");
+}
+else if ((xp >= 8001) && (xp < 9000)) {
+  console.log("O herói de nome " + nome + "e está no nível Ascendente");
+}
+else if ((xp >= 9001) && (xp < 10000)) {
+  console.log("O herói de nome " + nome + "e está no nível Imortal");
+}
+else{
+  console.log("O herói de nome " + nome + "e está no nível Radiante");
+}
+
